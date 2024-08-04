1530ee37ab733f1c749a711a1b03bdf7db5d172f
Ashwini Suryawanshi <141485991+suryawanshi666@users.noreply.github.com>
2024-08-04 22:13:12 +0530

    Create landmark.java

---
+++ landmark.java
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>Shrirampur city</title>
+  </head>
+  <body bgcolor="green">
+   <center>
+    <font color="blue" size="10">Shrirampur</font>
+   </center>
+   <h1>Landmark</h1>
+     <font color="cyne" size="6" face="gigi">
+       1) Belapur railway station
+      </font><br>
+      <font color="cyne" size="6" face="Time New Roman">
+      2)RBNB Collage
+      </font><br> 
+    <font color="cyne" size="6" face="gigi">
+      3)gandhi chauk
+      </font><br> 
+  </body>
+</html>
